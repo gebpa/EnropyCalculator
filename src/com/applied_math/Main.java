9bb031b06b47b023962190ce33349fb5ea36a1f6
gleb <gleber1995@gmail.com>
2018-09-19 18:00:23 +0300

    version to show

--- src/com/applied_math/Main.java
+++ src/com/applied_math/Main.java
@@ -1,13 +1,7 @@
 package com.applied_math;
 
-import javax.sound.midi.Soundbank;
 import java.io.*;
-import java.sql.SQLOutput;
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.regex.Matcher;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -15,7 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> frequency = new HashMap<>();
+        Map<String, Integer> pairFrequency = new HashMap<>();
         Pattern p = Pattern.compile("[a-z1-9 \r\n]");
         try {
             System.out.println("Input file name");
@@ -24,31 +19,73 @@ public class Main {
             FileReader reader = new FileReader(fileName);
             int c;
             int total = 0;
+            boolean isItFirst = true;
+            Character previousCh = ' ';
             while ((c = reader.read()) != -1) {
                 Character ch = Character.toLowerCase((char) c);
                 if (ch == '\n') continue;
                 if (!p.matcher(ch.toString()).matches()) {
                     ch = '.';
                 }
-                if (map.containsKey(ch)) {
-                    map.put(ch, map.get(ch) + 1);
+                if (frequency.containsKey(ch)) {
+                    frequency.put(ch, frequency.get(ch) + 1);
                 } else
-                    map.put(ch, 1);
+                    frequency.put(ch, 1);
+                if (!isItFirst) {
+                    String chain = ch + "" + previousCh;
+                    if (pairFrequency.containsKey(chain)) {
+                        pairFrequency.put(chain, pairFrequency.get(chain) + 1);
+                    } else
+                        pairFrequency.put(chain, 1);
+                } else
+                    isItFirst = false;
+                previousCh = ch;
                 total++;
             }
             int finalTotal = total;
-            Map<Character, Double> probability = map.entrySet()
+            Map<Character, Double> probability = frequency.entrySet()
                     .stream()
                     .map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey(), (double) entry.getValue() / finalTotal))
                     .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-            double entropy = 0;
-            System.out.println(1);
+            Map<Character, Double> symbolEntropy = probability.entrySet()
+                    .stream()
+                    .map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey(), (double) (-1) * Math.log(entry.getValue()) / Math.log(2)))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            Map<String, Double> probabilityChain = pairFrequency.entrySet()
+                    .stream()
+                    .map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey(), (double) entry.getValue() / frequency.get(entry.getKey().charAt(1))))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            Map<String, Double> chainEntropy = probabilityChain.entrySet()
+                    .stream()
+                    .map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey(), (double) (-1) * Math.log(entry.getValue()) / Math.log(2)))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            double symbolEntropyTotal = 0;
+            double chainEntropyTotal = 0;
+            System.out.println("Вероятность каждого символа:");
+            for (Character character : probability.keySet()) {
+                System.out.printf("%s: %.4f\n", character, probability.get(character));
+            }
+            System.out.println("Энтропия каждого символа:");
+            for (Character character : probability.keySet()) {
+                Double value = probability.get(character);
+                Double log2 = Math.log(value) / Math.log(2);
+                symbolEntropyTotal += value * log2;
+                System.out.printf("%s: %.4f\n", character, symbolEntropy.get(character));
+            }
+            for (String chain : probabilityChain.keySet()) {
+                Double value = probabilityChain.get(chain);
+                Double probabilityOfPrevious = probability.get(chain.charAt(1));
+                Double log2 = Math.log(value) / Math.log(2);
+                chainEntropyTotal += value * probabilityOfPrevious * log2;
+            }
+            chainEntropyTotal *= -1;
+            symbolEntropyTotal *= -1;
+            System.out.printf("Энтропия: %.4f\n", symbolEntropyTotal);
+            System.out.printf("Энтропия с использованием пар: %.4f", chainEntropyTotal);
         } catch (FileNotFoundException ex) {
             System.out.println("File not found");
         } catch (IOException ex) {
             System.out.println("Some IO exception");
         }
-
-
     }
 }
